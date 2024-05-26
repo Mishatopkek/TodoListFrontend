0b8c9614fd8632830d7800d9b000c3d2cbca257c
Mykhailo Bezuhlyi <bezuhlyi.mykhailo@gmail.com>
2024-05-26 21:57:32 +0300

    Fix cards now smaller

--- src/components/UI/JiraCard.jsx
+++ src/components/UI/JiraCard.jsx
@@ -1,41 +1,34 @@
-﻿import {Card, CardContent, Typography} from "@mui/material";
+﻿import {Card, Typography} from "@mui/material";
 import {Draggable} from "@hello-pangea/dnd";
 import Box from "@mui/material/Box";
 
 
 const JiraCard = ({card, index}) => {
-    return (
-        <Draggable
-            draggableId={card.id}
-            index={index}>
-            {(provided, snapshot) => (
-                <Box
-                    sx={{
-                        marginTop: '4px',
-                        marginDown: '4px',
-                        minWidth: 256,
-                        maxWidth: 256
-                    }}
-                    {...provided.draggableProps}
-                    {...provided.dragHandleProps}
-                    ref={provided.innerRef}>
-                    <Card>
-                        <CardContent>
-                            <Typography variant="body2" component="h2">
-                                {index} {card.title}
-                            </Typography>
-                            <Typography variant="body2" color="text.secondary">
-                                tag1, tag2
-                            </Typography>
-                            <Typography variant="overline" color="cyan">
-                                ❤️ KAN-123
-                            </Typography>
-                        </CardContent>
-                    </Card>
-                </Box>
-            )}
-        </Draggable>
-    );
+    return (<Draggable
+        draggableId={card.id}
+        index={index}>
+        {(provided, snapshot) => (<Box
+            sx={{
+                marginTop: '4px', marginDown: '4px', minWidth: 256, maxWidth: 256
+            }}
+            {...provided.draggableProps}
+            {...provided.dragHandleProps}
+            ref={provided.innerRef}>
+            <Card sx={{
+                padding: '8px',
+            }}>
+                <Typography variant="body2" component="h2">
+                    {index} {card.title}
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                    tag1, tag2
+                </Typography>
+                <Typography variant="overline" color="cyan">
+                    ❤️ KAN-123
+                </Typography>
+            </Card>
+        </Box>)}
+    </Draggable>);
 };
 
 export default JiraCard;
