2e898aadd0032e56d68525454cde782eab59a3a5
Mykhailo Bezuhlyi <bezuhlyi.mykhailo@gmail.com>
2024-08-02 22:08:31 +0300

    Add react router and skeleton for possible pages

---
+++ src/pages/Board.jsx
@@ -0,0 +1,119 @@
+﻿import {createTheme, CssBaseline, ThemeProvider, Typography} from "@mui/material";
+import {useCallback, useState} from "react";
+import {useDispatch, useSelector} from "react-redux";
+import {boardActions} from "../store/boards.js";
+import MiniDrawer from "../components/UI/Drawer.jsx";
+import Box from "@mui/material/Box";
+import {DragDropContext, Droppable} from "@hello-pangea/dnd";
+import Column from "../components/UI/Column.jsx";
+import CreateColumn from "../components/UI/Column/CreateColumn.jsx";
+import IconButton from "@mui/material/IconButton";
+import AddIcon from "@mui/icons-material/Add.js";
+
+const Board = () => {
+    const darkTheme = createTheme({
+        palette: {
+            mode: 'dark',
+        },
+    });
+    const [showCreateColumn, setShowCreateColumn] = useState(false);
+    const dispatch = useDispatch();
+    const board = useSelector(state => state.board);
+    const onIconButtonClick = useCallback((event) => {
+        setShowCreateColumn(true);
+    }, []);
+
+    const onOutsideClick = useCallback(() => {
+        setShowCreateColumn(false);
+    }, []);
+
+    const onCancelColumn = useCallback(() => {
+        setShowCreateColumn(false);
+    }, []);
+
+    const onSubmitColumn = useCallback((title) => {
+        if (title.length < 3) {
+            return;
+        }
+
+        dispatch(boardActions.addColumn({
+            id: crypto.randomUUID(),
+            title: title,
+            cards: []
+        }));
+        setShowCreateColumn(false);
+    }, [dispatch]);
+
+    return (
+        <ThemeProvider theme={darkTheme}>
+            <MiniDrawer/>
+            <main style={{
+                display: 'block',
+                overflowY: 'auto',
+                height: '100%'
+            }}>
+                <Box style={{
+                    display: 'block',
+                    overflowY: 'auto',
+                    paddingTop: 64,
+                    paddingLeft: 64 + 64, // left drawer + extra space
+                    height: '100%',
+                }}>
+                    <CssBaseline/>
+                    <Box marginTop={4}>
+                        <Typography variant="h6"
+                                    component="h6">https://mui.com/material-ui/react-breadcrumbs/</Typography>
+                    </Box>
+                    <Box marginTop={4}>
+                        <Typography variant="h4" component="h1">{board.title}</Typography>
+                    </Box>
+                    <DragDropContext onDragEnd={dropResult => {
+                        if (dropResult.source === null || dropResult.destination === null) return columns;
+
+                        // Column reorder
+                        if (dropResult.source.droppableId === dropResult.destination.droppableId && dropResult.source.droppableId === "board") {
+                            dispatch(boardActions.updateColumnPosition(dropResult));
+                        } else {
+                            // Card move
+                            dispatch(boardActions.updateCardPositionInColumn(dropResult));
+                        }
+                    }}>
+                        <Droppable
+                            droppableId="board"
+                            type="COLUMN"
+                            direction="horizontal"
+                            // ignoreContainerClipping={true}
+                            sx={{display: 'flex', height: '100%'}}
+                        >
+                            {provided => (
+                                <Box
+                                    ref={provided.innerRef}
+                                    {...provided.droppableProps}
+                                    sx={{display: 'flex', height: '100%'}}
+                                >
+                                    {board.columns.map((column, index) =>
+                                        <Column key={column.id} index={index} column={column}/>
+                                    )}
+                                    {provided.placeholder}
+
+                                    <Box>
+                                        {showCreateColumn &&
+                                            <CreateColumn
+                                                onSubmit={onSubmitColumn}
+                                                onCancel={onCancelColumn}
+                                                onOutsideClick={onOutsideClick}
+                                            />}
+                                        {!showCreateColumn && <IconButton aria-label="add" onClick={onIconButtonClick}>
+                                            <AddIcon/>
+                                        </IconButton>}
+                                    </Box>
+                                </Box>
+                            )}
+                        </Droppable>
+                    </DragDropContext>
+                </Box>
+            </main>
+        </ThemeProvider>
+    );
+};
+export default Board;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,16 @@
+﻿// import {NavLink} from "react-router-dom";
+//
+// const Home = () => {
+//     return (
+//         <NavLink
+//             to="/"
+//             className={({isActive}) =>
+//                 isActive ? classes.active : undefined
+//             }
+//             end
+//         >
+//             Home
+//         </NavLink>
+//     );
+// };
+// export default Home;
