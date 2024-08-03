7463794c4c1dc019c4930da41b6b173f545d6866
Mykhailo Bezuhlyi <bezuhlyi.mykhailo@gmail.com>
2024-08-03 14:08:00 +0300

    Add cards to a home page

---
+++ src/components/UI/Home/BoardCard.jsx
@@ -0,0 +1,36 @@
+﻿import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
+import {CardActionArea} from "@mui/material";
+
+
+const BoardCard = ({title, onClick}) => {
+    return <Card sx={{maxWidth: 250}}>
+        <CardActionArea onClick={onClick}>
+            <CardContent>
+                <Typography variant="h5" component="div">
+                    {title}
+                </Typography>
+                <Typography sx={{mb: 1.5}} color="text.secondary">
+                    Team-managed software
+                </Typography>
+                <Typography sx={{fontSize: 14}} color="text.secondary" gutterBottom>
+                    QUICK LINKS
+                </Typography>
+                <Typography sx={{fontSize: 14}} color="text.secondary" gutterBottom>
+                    My open issue
+                </Typography>
+                <Typography sx={{fontSize: 14}} color="text.secondary" gutterBottom>
+                    Done issues
+                </Typography>
+            </CardContent>
+            <CardActions>
+                <Typography>
+                    1 board
+                </Typography>
+            </CardActions>
+        </CardActionArea>
+    </Card>;
+}
+export default BoardCard;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,27 @@
-﻿import {Link} from "react-router-dom";
+﻿import {Link, useNavigate} from "react-router-dom";
+import Box from "@mui/material/Box";
+import {Typography} from "@mui/material";
+import Divider from '@mui/material/Divider';
+import BoardCard from "../components/UI/Home/BoardCard.jsx";
+import {useCallback} from "react";
 
 const Home = () => {
+    const navigate = useNavigate();
+    const handleCardClick = useCallback(() => {
+        navigate("/misha/testboard")
+    }, [navigate]);
     return (
-        <div>
-            <ul>
-                <li><Link to="misha">Test Misha page</Link></li>
-                <li><Link to="misha/testboard">Test Misha board</Link></li>
-            </ul>
-        </div>
+        <>
+            <Typography variant="h2">Your work</Typography>
+            <Divider/>
+            <Typography variant="h3">Recent projects</Typography>
+            <BoardCard title="My Kanban Project" onClick={handleCardClick}/>
+            <Box>
+                <ul>
+                    <li><Link to="misha">Test Misha page</Link></li>
+                </ul>
+            </Box>
+        </>
     );
 };
 export default Home;
