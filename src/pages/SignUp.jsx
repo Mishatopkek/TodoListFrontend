80cacb465d93b80b82ecfd66f93bb7862c9f5dc2
Mykhailo Bezuhlyi <bezuhlyi.mykhailo@gmail.com>
2024-08-11 15:37:21 +0300

    Add SignUp in backend and frontend Add a requirement to add jwtSecret during the app's run Add an extension for converting Ardalis to FluentValidation errors Add FluentValidation global language as English Add CORSes Update all packages because of bugs in one of them Fix the "extensions" type Fix config export

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,4 +1,5 @@
 ﻿import * as React from 'react';
+import {useCallback, useState} from 'react';
 import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
 import CssBaseline from '@mui/material/CssBaseline';
@@ -12,18 +13,61 @@ import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import BlackTheme from "../components/wrappers/BlackTheme.jsx";
-import {Link} from "react-router-dom";
+import {Link, useLocation, useNavigate} from "react-router-dom";
+import {authActions} from "../store/auth.js";
+import {useDispatch} from "react-redux";
+import config from "../../config.js";
 
 export default function SignUp() {
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+    const location = useLocation();
+    // const [afterSignUp, setAfterSignUp] = useState(false);
+    const [validationErrors, setValidationErrors] = useState({});
+
     const handleSubmit = (event) => {
         event.preventDefault();
         const data = new FormData(event.currentTarget);
-        console.log({
+        const userData = {
             name: data.get('username'),
             email: data.get('email'),
             password: data.get('password'),
-        });
+        };
+
+        fetch(`${config.backendUrl}/api/User/SignUp`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(userData)
+        })
+            .then(response => {
+                if (response.status === 201) {
+                    return response.json().then(data => {
+                        const token = data.token;
+                        localStorage.setItem('token', token);
+                        dispatch(authActions.loginSuccess(token));
+
+                        const from = location.state?.from?.pathname || "/";
+                        navigate(from, {replace: true});
+                    });
+                } else if (response.status === 400 || response.status === 409) {
+                    return response.json().then(data => {
+                        setValidationErrors(data.errors);
+                    });
+                } else {
+                    // Handle other status codes
+                    console.error('Unexpected status code:', response.status);
+                }
+            });
     };
+
+    const onInputChange = useCallback((inputName) => {
+        setValidationErrors(prevState => {
+            delete prevState[inputName];
+            return prevState;
+        })
+    }, []);
 
     return (
         <BlackTheme>
@@ -53,6 +97,9 @@ export default function SignUp() {
                                     label="Username"
                                     name="username"
                                     autoComplete="username"
+                                    onChange={() => onInputChange("name")}
+                                    error={!!validationErrors.name}
+                                    helperText={!!validationErrors.name && validationErrors.name[0]}
                                 />
                             </Grid>
                             <Grid item xs={12}>
@@ -63,6 +110,9 @@ export default function SignUp() {
                                     label="Email Address"
                                     name="email"
                                     autoComplete="email"
+                                    onChange={() => onInputChange("email")}
+                                    error={!!validationErrors.email}
+                                    helperText={!!validationErrors.email && validationErrors.email[0]}
                                 />
                             </Grid>
                             <Grid item xs={12}>
@@ -74,6 +124,9 @@ export default function SignUp() {
                                     type="password"
                                     id="password"
                                     autoComplete="new-password"
+                                    onChange={() => onInputChange("password")}
+                                    error={!!validationErrors?.password}
+                                    helperText={!!validationErrors.password && validationErrors.password[0]}
                                 />
                             </Grid>
                             <Grid item xs={12}>
