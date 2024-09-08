1df479068d0de54c3b5f2888ba3d94b8094921dc
Mykhailo Bezuhlyi <bezuhlyi.mykhailo@gmail.com>
2024-09-08 18:23:04 +0300

    Add support a list of boards in a main page

---
+++ src/api/Boards/BoardList.js
@@ -0,0 +1,12 @@
+﻿import config from "../../../config.js";
+
+const FetchBoardList = async (jwtToken) => {
+    return await fetch(`${config.backendUrl}/api/Board`, {
+        method: 'GET',
+        headers: {
+            'Authorization': 'Bearer ' + jwtToken
+        }
+    });
+}
+
+export default FetchBoardList;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,18 +3,22 @@ import Box from "@mui/material/Box";
 import {Typography} from "@mui/material";
 import Divider from '@mui/material/Divider';
 import BoardCard from "../components/UI/Home/BoardCard.jsx";
-import {useCallback, useState} from "react";
+import {useCallback, useEffect, useState} from "react";
 import {useSelector} from "react-redux";
+import FetchBoardList from "../api/Boards/BoardList.js";
+import Grid from "@mui/material/Grid";
 
 const Home = () => {
     const navigate = useNavigate();
     const auth = useSelector(state => state.auth);
-    const [projects, setProjects] = useState([{
-        id: "B6DED1FA-53E2-4EA6-9793-2E37E9920774",
-        name: "SuperMisha",
-        title: "The board full of Misha",
-        author: "Misha"
-    }]);
+    const [projects, setProjects] = useState([]);
+    useEffect(() => {
+        if (auth.isAuthenticated) {
+            FetchBoardList(auth.token)
+                .then(x => x.json())
+                .then(x => setProjects(x));
+        }
+    }, []);
     const handleCardClick = useCallback((project) => {
         const {author, name} = project;
         const url = `/${author}/${name}`;
@@ -25,9 +29,13 @@ const Home = () => {
             <Typography variant="h2">Your work</Typography>
             <Divider/>
             <Typography variant="h3">Recent projects</Typography>
-            {projects.map(project => (
-                <BoardCard key={project.id} title={project.title} onClick={() => handleCardClick(project)}/>
-            ))}
+            <Grid container spacing={2}>
+                {projects.map(project => (
+                    <Grid item key={project.id}>
+                        <BoardCard title={project.title} onClick={() => handleCardClick(project)}/>
+                    </Grid>
+                ))}
+            </Grid>
             <Box>
                 <ul>
                     <li><Link to="misha">Test Misha page</Link></li>
